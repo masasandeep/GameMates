6ab9c94bdfb84faed61cf04675157874bc79e37e
gunavardhan27 <bgunavardhan27@gmail.com>
2024-08-26 01:22:17 +0530

    authentication-functionality-added

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -3,11 +3,33 @@ import Modal from "./Modal"
 import store, { loginAction } from "./store/LoginContext"
 export default function Login(){
     const toggle = useSelector(state=>state.log.show)
-    const handleSubmit = (e)=>{
+    const handleSubmit = async (e)=>{
         e.preventDefault()
         const fd = new FormData(e.target)
         const data = Object.fromEntries(fd.entries())
-        console.log(data)
+        console.log(data.email,data.password)
+        const response = await fetch('http://localhost:5000/api/users/login',
+            {
+                method:'POST',
+                headers:{
+                    'Content-Type':'application/json'
+                },
+                body:JSON.stringify({
+                email:data.email,
+                password:data.password,  
+                })
+            }
+        )
+        if (!response.ok) {
+            // If the response is not OK, handle the error
+            const errorData = await response.json();
+            console.error('Error:', errorData.message);
+          } else {
+            // If the response is OK, handle the data
+            const responseData = await response.json();
+            console.log('Success:', responseData);
+          }
+        store.dispatch(loginAction.toggle())
 
     }
     return (            
@@ -20,17 +42,17 @@ export default function Login(){
             <form className='w-full flex flex-col items-center justify-center' onSubmit={handleSubmit}>
             <div className='flex flex-col w-full items-center justify-center'>
                 <label htmlFor="name" className="mb-2 text-white font-medium">
-                    Username :
+                    Email :
                 </label>
-                <input type='text' name="username" className='p-2 border w-[80%] border-gray-300 rounded-2xl focus:outline-none focus:ring-2
-                    focus:ring-blue-500' />
+                <input type='email' name="email" className='p-2 border w-[80%] border-gray-300 rounded-2xl focus:outline-none focus:ring-2
+                    focus:ring-blue-500' required />
                 </div>
             <div className='flex flex-col w-full items-center justify-center'>
                 <label htmlFor="name" className="mb-2 text-white font-medium">
                     Password :
                 </label>
                 <input type='password' name="password" className='p-2 border w-[80%] border-gray-300 rounded-2xl focus:outline-none focus:ring-2
-                    focus:ring-blue-500' />
+                    focus:ring-blue-500' required />
             </div>
             <button type="submit" className="m-2 text-lg text-white bg-cyan-600 p-3 rounded-lg">Log-In</button>
             </form>
--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,34 +1,63 @@
+
+
 export default function Register(){
+    const handleRegister= async (e)=>{
+        e.preventDefault()
+        const fd = new FormData(e.target)
+        const data = Object.fromEntries(fd.entries())
+        const response = await fetch('http://localhost:5000/api/users/registerUser',{
+            method:'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify({
+                name:data.name,
+                email:data.email,
+                password:data.password,
+                confirmPassword:data.confirmPassword
+            })
+        })
+        if(response.ok){
+            const responseData = await response.json()
+            console.log(responseData)
+        }
+        else{
+            const errorData = await response.json()
+            console.log(errorData)
+        }
+
+
+    }
     return (
         <div className='flex flex-row items-center justify-center m-3'>
         <div className='w-[50%] bg-slate-500 p-5'>
-            <form className='w-full flex flex-col items-left justify-center'>
+            <form className='w-full flex flex-col items-left justify-center' onSubmit={handleRegister}>
             <div className='grid gird-cols-2'>
                 <label htmlFor="name" className="mb-2 text-gray-700 font-medium">
                     Username :
                 </label>
-                <input type='text' className='p-2 border border-gray-300 rounded-2xl focus:outline-none focus:ring-2
+                <input type='text' name="name" className='p-2 border border-gray-300 rounded-2xl focus:outline-none focus:ring-2
                     focus:ring-blue-500' />
                 </div>
             <div className='grid gird-cols-2'>
                 <label htmlFor="name" className="mb-2 text-gray-700 font-medium">
                     Email :
                 </label>
-                <input type='email' className='p-2 border border-gray-300 rounded-2xl focus:outline-none focus:ring-2
+                <input type='email' name="email" className='p-2 border border-gray-300 rounded-2xl focus:outline-none focus:ring-2
                     focus:ring-blue-500' />
             </div>
             <div className='grid gird-cols-2'>
                 <label htmlFor="name" className="mb-2 text-gray-700 font-medium">
                     Password :
                 </label>
-                <input type='password' className='p-2 border border-gray-300 rounded-2xl focus:outline-none focus:ring-2
+                <input type='password' name="password" className='p-2 border border-gray-300 rounded-2xl focus:outline-none focus:ring-2
                     focus:ring-blue-500' />
             </div>
             <div className='grid gird-cols-2'>
                 <label htmlFor="name" className="mb-2 text-gray-700 font-medium">
                     Confirm Password :
                     </label>
-                    <input type='text' className='p-2 border border-gray-300 rounded-2xl focus:outline-none focus:ring-2
+                    <input type='text' name="confirmPassword" className='p-2 border border-gray-300 rounded-2xl focus:outline-none focus:ring-2
                        focus:ring-blue-500' />
             </div>
             <div className='flex flex-row items-center justify-center'>
